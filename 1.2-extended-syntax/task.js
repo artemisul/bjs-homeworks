cd2b86421fcdbaab5c6b39feeea8536be5864f83
Игорь Головатый <i.golovatyy@mangotele.com>
2021-04-20 13:01:55 +0300

    Задание 1 - Готово

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,24 @@
-function getResult(a,b,c){
+'use strict'
+function getResult(a, b, c) {
     // код для задачи №1 писать здесь
+    let diskriminant = b * b - 4 * a * c;
+    
+    if (diskriminant > 0) {
+        let firstX = ((-b) + Math.sqrt(diskriminant)) / 2 * a;
+        let secondX = ((-b) - Math.sqrt(diskriminant)) / 2 * a;
+        let x = [firstX, secondX];
+        return x;
+    } else if (diskriminant < 0) {
+        let x = [];
+        return x;
+    } else {
+        let firstX = (-b) / 2 * a;
+        let x = [firstX]
+        return x;
+    }
     // return x;
+    
+    
 }
 
 function getAverageMark(marks){
