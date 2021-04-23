364bcf27c9dd11bda96ea5819ada4edf57e4b65b
Игорь Головатый <i.golovatyy@mangotele.com>
2021-04-23 09:16:17 +0300

    Занятие 2.3 Задача 2 - Готово

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,3 +1,7 @@
+'use strict';
+
+// Задача 1
+
 class PrintEditionItem {
     constructor (name, releaseDate, pagesCount) {
         this.name = name;
@@ -62,3 +66,57 @@ class DetectiveBook extends Book {
     }
     type = 'detective';
 }
+
+// Задача 2
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+    addBook(book) {
+        let state = book.state;
+        if (state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    set books(value) {
+        this._books = value;
+    }
+
+    get books() {
+        return this._books;
+    }
+
+    findBookBy(type, value) {
+        for (let i = 0; i < this.books.length; i++) {
+            for (type in this.books[i]) {
+                let bookCase = this.books[i];
+                let valueBook = bookCase[type];
+                if (valueBook === value) {
+                    return bookCase;
+                }
+            }
+        }
+        return null;
+    }
+
+    giveBookByName(bookName) {        
+        for (let i = 0; i < this.books.length; i++) {
+            for (this.name in this.books[i]) {
+                let bookCase = this.books[i];
+                let valueBook = bookCase[this.name];
+                if (valueBook === bookName) {
+                    this.books.splice(i, 1);
+                    return bookCase;
+                }
+            }
+        }
+        return null;
+    }
+
+    
+}
+
+
